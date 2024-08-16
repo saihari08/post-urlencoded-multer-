5dca02072707a47902640c96ec83b0f6df2106cf
saihari08 <bsaihari08@gmail.com>
2024-08-16 15:44:18 +0530

    Add files via upload

---
+++ post-urlencoded-multer-/client/src/components/Signup.js
@@ -0,0 +1,198 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Signup() {
+    let firstNameInputRef=useRef();
+    let lastNameInputRef=useRef();
+    let stateSelectRef=useRef();
+    let emailInputRef=useRef();
+    let passwordInputRef=useRef();
+    let phoneInputRef=useRef();
+    let ProfilePicInputRef=useRef();
+    let msgLabelRef=useRef();
+    let salutation;
+    let ageinputRef=useRef();
+    
+ let [ProfilePic,setProfilePic]=useState("./images/noimage.png");   
+let onCreateAccountClick=async()=>{
+     let DataToSend={
+        firstName:firstNameInputRef.current.value,
+        lastName:lastNameInputRef.current.value,
+        age:ageinputRef.current.value,
+        email:emailInputRef.current.value,
+        password:passwordInputRef.current.value,
+        ProfilePic:ProfilePicInputRef.current.value,
+        phone:phoneInputRef.current.value,
+        };
+let JSONDataToSend=JSON.stringify(DataToSend);
+    console.log(DataToSend);
+    console.log(JSONDataToSend);
+
+let myHeader=new Headers();
+myHeader.append("content-type","application/json");
+
+let reqOption={
+    method:"POST",
+    body:JSONDataToSend,
+    header:myHeader,
+};
+let JSONData=await fetch("http://localhost:2120/signup",reqOption);
+let JSOData=await JSONData.json();
+alert(JSOData.msg);
+console.log(JSOData);
+};
+
+let onCreateAccountClickURLE=async()=>{
+    let myHeader=new Headers();
+    myHeader.append("content-type","application/x-www-form-urlencoded");
+
+    let DataToSend=new URLSearchParams()
+DataToSend.append("firstName",firstNameInputRef.current.value)
+DataToSend.append("lastName",lastNameInputRef.current.value)
+DataToSend.append("age",ageinputRef.current.value)
+DataToSend.append("email",emailInputRef.current.value)
+DataToSend.append("password",passwordInputRef.current.value)
+DataToSend.append("phoneNo",phoneInputRef.current.value)
+DataToSend.append("profilePic",ProfilePicInputRef.current.value)
+
+    let reqOption={
+        method:"POST",
+        headers:myHeader,
+        body:DataToSend,
+    };
+
+    let JSONData=await fetch("http://localhost:2120/signup",reqOption);
+    let JSOData=await JSONData.json();
+    alert(JSOData.msg);
+    console.log(JSOData);
+};
+
+let onCreateAccountClickFD=async()=>{
+
+    let DataToSend=new FormData()
+DataToSend.append("firstName",firstNameInputRef.current.value)
+DataToSend.append("lastName",lastNameInputRef.current.value)
+DataToSend.append("age",ageinputRef.current.value)
+DataToSend.append("email",emailInputRef.current.value)
+DataToSend.append("password",passwordInputRef.current.value)
+DataToSend.append("phoneNo",phoneInputRef.current.value)
+//DataToSend.append("profilePic",ProfilePicInputRef.current.value)
+
+DataToSend.append("profilePic",ProfilePicInputRef.current.files[0]);
+for (let i = 0; i < ProfilePicInputRef.current.files.length; i++) {
+    DataToSend.append(
+      "profilePic",
+      ProfilePicInputRef.current.files[i]);}
+    let reqOption={
+        method:"POST",
+        body:DataToSend,
+    };
+
+    let JSONData=await fetch("http://localhost:2120/signup",reqOption);
+    let JSOData=await JSONData.json();
+    alert(JSOData.msg);
+    console.log(JSOData);
+}
+
+
+  return (
+<div className='App'>
+<form>
+    <h2 style={{backgroundColor:"grey",borderRadius:"25px",color:"black",
+      boxShadow:"10px 10px 10px black"}}>Signup</h2>
+    
+        <div> 
+        <input ref={firstNameInputRef} type="text" placeholder='FirstName'></input>
+        <input ref={lastNameInputRef} type="text" placeholder='lastName'></input>
+        </div>   
+
+    <div> 
+     <input type="text" placeholder='email'ref={emailInputRef} ></input>
+     <input type="text" placeholder='password'ref={passwordInputRef}></input>
+    </div>
+    
+    <div> 
+    <input type="text" placeholder='phone-no'ref={phoneInputRef}></input>
+
+        <ladel>State</ladel>
+        <select ref={stateSelectRef}>
+            <option value="AP">Andhra Pradesh</option>
+            <option>Telangana</option>
+            <option>Karnataka</option>
+            <option value="TN">TamilNadu</option>
+            <option>Kerala</option>
+        </select>
+        </div>
+
+    <div>   
+     
+    <input type="text" placeholder='address'></input>
+
+    
+    <input ref={ageinputRef} type="text" placeholder='age' onChange={()=>{
+            let age=Number(ageinputRef.current.value);
+
+            if(age <5){
+                console.log(`infant`);
+            } else 
+            if(age>=5 && age <=10){
+                console.log(`kid`);
+            } else 
+            if(age>10 && age<=15){
+                console.log(`Teen`);
+            } else 
+            if(age >15 && age <25){
+                console.log(`youth`);
+            } else 
+            if(age >30 && age <50){
+                console.log(`middle aged`)
+            }else 
+            if(age >50 && age <70){
+                console.log(`old aged`)
+            }else
+                console.log(`not a valid age`);
+        }}></input>
+   </div>
+     
+        
+   <div>
+    <label>ProfilePic</label>
+    <input ref={ProfilePicInputRef}  type="file" accept="image/*"onChange={(eo)=>{
+        console.log(eo.target.files);
+
+    let selectedimagepath=URL.createObjectURL(eo.target.files[0]);
+    setProfilePic(selectedimagepath);
+    console.log(selectedimagepath);
+    }}></input>
+    <br></br>
+    <img src={ProfilePic} className="profileviewer"></img>
+   </div>
+
+
+<div> 
+<button type="button" onClick={()=>{
+    onCreateAccountClick();
+}}>Signup</button>
+
+
+<button type="button" onClick={()=>{
+    onCreateAccountClickURLE();
+}}>Signup(URLE)</button>
+
+<button type="button" onClick={()=>{
+   onCreateAccountClickFD();
+}}>Signup(FD)</button>
+</div>
+
+<div>
+    <label ref={msgLabelRef}></label>
+</div>
+
+<br></br>
+    <Link to="/">Signin</Link> 
+</form>
+</div>
+  )
+}
+
+export default Signup
